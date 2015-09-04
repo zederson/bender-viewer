7b7996eba6276e0334ffa6bd6b6a6f6abde12bb3
Ederson de Lima <ederson.lima@locaweb.com.br>
2015-09-04 16:22:52 -0300

    first commit

---
+++ services/publisher_web.js
@@ -0,0 +1,20 @@
+var mqtt      = require('mqtt');
+var publisher = require('./publisher_web');
+var host      = 'mqtt://cpro25389.publiccloud.com.br';
+var subscribe = ['sensors/temperature', 'sensors/luminosity'];
+var client    = mqtt.connect(host);
+
+
+module.exports.start = function(io) {
+  client.on('connect', function () {
+    subscribe.forEach(function(item){
+      client.subscribe(item);
+    });
+  });
+
+  io.on('connection', function (socket) {
+    client.on('message', function (topic, message) {
+      socket.emit(topic, { value: message });
+    });
+  });
+}
